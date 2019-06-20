a3990c43ab4108c167203592c6a22d9f49f2fd3a
BuildTools <nooaandersson@github.vom>
2019-06-20 16:09:23 +0200

    json

--- javascript.js
+++ javascript.js
@@ -16,11 +16,20 @@ function StringSearch() {
     window.alert(SearchTerm);
     window.open("https://api.github.com/users/" + SearchTerm);
 
+    $.getJSON('https://api.github.com/users/' + SearchTerm, function (data) {
+        console.log("JSON Data: " + data.url);
+
+    });
+    
 }
 
 function button() {
     var a = SearchTerm;
     window.alert("well that worked")
-    window.open("https://api.github.com/users/" + a)
+    //window.open("https://api.github.com/users/" + a)
+
+    $.getJSON('https://api.github.com/users/' + a, function (data) {
+        window.alert("funka")
+    });
 
 }
