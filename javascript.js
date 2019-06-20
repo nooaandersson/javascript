e7a114c4a1b0cfa3e06a1306c40446182d2913ab
BuildTools <nooaandersson@github.vom>
2019-06-20 03:08:37 +0200

    l.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
-let a; 
-let b;
 function main() {
 
 }
 
+
+function hello() {
+    var e = document.getElementById("text").innerHTML;
+    if (e == "hej") {
+        window.alert("bla");
+    }
+}  
+
+
+function StringSearch() {
+    var SearchTerm = document.getElementById("text_box_1").value;
+    var TextSearch = document.getElementById("text_area_3").value;
+    if (SearchTerm.length > 0 && TextSearch.indexOf(SearchTerm) > -1) {
+        alert("String Found. Search Complete");
+    };
+    window.alert(SearchTerm);
+    window.open("https://api.github.com/users/" + SearchTerm);
+
+}
+
 function button() {
+    var a = SearchTerm;
     window.alert("well that worked")
-    window.open("http://spelklubben.eu")
+    window.open("https://api.github.com/users/" + a)
 
 }
